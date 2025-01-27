54434dfdec3a0df728d72dee67e8902b57c66319
Blazej <lubieboea@gmail.com>
2025-01-27 17:06:38 +0100

    lekcja faktycznie teraz

--- lekcja1.py
+++ lekcja1.py
@@ -3,7 +3,7 @@ hostname = socket.gethostname() #pobieranie nazwy hosta
 print (f"hello {hostname}!") #wyswietlenie nazwy hosta 
 name = input("Jak masz na imie? ") #pytanie o imie (pobranie danych od uzytkownika)
 age = int(input("Ile masz lat? ")) #pytanie o wiek (pobranie danych od uzytkownika) int(input())- konwertuje na liczbe calkowita  
-if age >= 18: 
+if age >= 18: #sprawdza czy warunek jest spelniony
     print("Jestes pelnoletni!")
-else:
+else: #jesli warunek nie zostanie spelniony 
     print("Jestes niepelnoletni!")
